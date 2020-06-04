464f11d1130a25b60d7373d5c80e4f241cf6c08d
Schwamii <joecool3336@gmail.com>
2020-06-04 11:32:05 -0400

    Fixing issues #1 and #2
    added TryParse while loops to the first 2 questions so you don't get thrown an exception if you enter incorrect characters

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 
 namespace ConsoleApp1
@@ -28,28 +29,50 @@ namespace ConsoleApp1
             */
 
             //------------------------- Initialization ------------------------- 
-            string repeat; //Number of times to repeat
-            string rngInitSeed; //Initial seed
+            string repeat = ""; //Number of times to repeat
+            string rngInitSeed = ""; //Initial seed
             string rngBaseNumOne = "41C64E6D"; //First number in RNG equation
             string rngBaseNumTwo = "6073"; //Second fixed number in RNG equation
-            bool critSearch = false; //Initializes a bool for the question of whether to search for crit frames
+            bool critSearch = false; //Initializes a boolean for the question of whether to search for crit frames
             string critAsk; //Initializes the string that will be read from to set critSearch
-            bool rollSearch = false; //Initializes a bool for the question of whether to search for rolls after crits
+            bool rollSearch = false; //Initializes a boolean for the question of whether to search for rolls after crits
             string rollAsk; //Initializes the string that will be read from to set rollSearch
             int subCalc; //Initializes an integer that's used in the rollSearch loops
             int subLoopCount = 1; //Initializes another integer that's used in the rollSearch loops
             string subHex = "FFFFFFFF"; //Initializes a string that's used in the rollSearch loops
             int finalFrame; //Initializes an integer that's used if a match is found on the rollSearch loops
+            int parseAttempt; //Initializes an integer that's mandatory for TryParse
+            CultureInfo provider = CultureInfo.InvariantCulture; //Creates an IFormatProvider variable so TryParse can work with the hex values on initial seed input
+            bool initSeedParse = false; //Initializes a boolean for the TryParse while loop for the initial seed
+            bool repeatInputParse = false; //Initializes a boolean for the TryParse while loop for the number of times to repeat
             //------------------------- End of initialization ------------------------- 
 
 
 
             //------------------------- Questions ------------------------- 
-            Console.WriteLine("Enter the initial seed hex value: "); //Self-Explanatory
-            rngInitSeed = Console.ReadLine(); //Sets initial seed to the value that was input
+            while (initSeedParse == false) //Makes sure that as long as you enter characters that are not hex numbers you will be asked this question again
+            {
+                Console.WriteLine("Enter the initial seed hex value: "); //Self-Explanatory
+                rngInitSeed = Console.ReadLine(); //Sets initial seed to the value that was input
+                initSeedParse = int.TryParse(rngInitSeed, NumberStyles.HexNumber, provider, out parseAttempt); //Attempts to parse the input, setting initSeedParse to true if it succeeds
+                if (initSeedParse == false)
+                {
+                    Console.WriteLine("Please enter a hex value.");
+                    Console.WriteLine();
+                }
+            }
 
-            Console.WriteLine("Enter number of times to repeat: "); //Self-Explanatory
-            repeat = Console.ReadLine(); //Gets number of times to repeat
+            while (repeatInputParse == false) //Makes sure that as long as you enter characters that are not decimals you will be asked this question again
+            {
+                Console.WriteLine("Enter number of times to repeat: "); //Self-Explanatory
+                repeat = Console.ReadLine(); //Gets number of times to repeat
+                repeatInputParse = int.TryParse(repeat, out parseAttempt); //Attempts to parse the input, setting repeatInputParse to true if it succeeds
+                if (repeatInputParse == false)
+                {
+                    Console.WriteLine("Please enter an integer.");
+                    Console.WriteLine();
+                }
+            }
 
             Console.WriteLine("Would you like to search for only crit frames?"); //Self-Explanatory
             critAsk = Console.ReadLine(); //Gets answer for crit frame question
@@ -73,9 +96,9 @@ namespace ConsoleApp1
 
             //------------------------- Calculation and display ------------------------- 
             int repeatTimes = Int32.Parse(repeat); //Sets an integer equal to the parsed value of repeat
-            int InitSeed = Int32.Parse(rngInitSeed, System.Globalization.NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngInitSeed
-            int BaseNumOne = Int32.Parse(rngBaseNumOne, System.Globalization.NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumOne
-            int BaseNumTwo = Int32.Parse(rngBaseNumTwo, System.Globalization.NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumTwo
+            int InitSeed = Int32.Parse(rngInitSeed, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngInitSeed
+            int BaseNumOne = Int32.Parse(rngBaseNumOne, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumOne
+            int BaseNumTwo = Int32.Parse(rngBaseNumTwo, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumTwo
             int firstCalc = BaseNumOne * InitSeed + BaseNumTwo; //Calculates the first RNG result
             int repeated = 1; //Initializes the amount of times the loop has been repeated
             string hexResult = firstCalc.ToString("X8"); //Converts firstCalc back to hex
