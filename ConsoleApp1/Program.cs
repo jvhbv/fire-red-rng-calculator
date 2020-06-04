60752aae7d5dcd0a1f56b6f8e6ebed9f3ddbd935
Schwamii <joecool3336@gmail.com>
2020-06-04 08:46:44 -0400

    Optimizing some if statements and other stuff
    I also added an explanation of how the if (var.IndexOf(string, offset, length) == offset){} expression used multiple times in the program works, mainly for my own reference, as well as more comments explaining how the calculator handles searching for crit roll combos, just to make the code more readable and pleasant to look at.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,22 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             /*
-            Possible future feature implementations-
+             Possible future feature implementations-
 
-            Finding only pairs of frames that give both a crit and max damage roll - Added
-            Adding a way to print only frames after a given RNG frame
+             Finding only pairs of frames that give both a crit and max damage roll - Added
+             Adding a way to print only frames after a given RNG frame
+            */
+
+
+            /*
+             The way the
+             if (var.IndexOf(string, offset, length) == offset){}
+             function works is that it looks at a specific variable (var in the example), then it checks if a specific set of characters (string in the example) in an index location of the
+             variable (offset in the example) for a specific amount of characters (length in the example) is actually at the index location (offset in the example).
+
+             Because of how it's set up, all it cares about is the contents of string within the bounds of length when performing the check, if string is not at said index location (i.e. a 
+             different set of characters is at the index location of the variable in question) then it will return false, as the string at that offset for that length is not actually at the 
+             offset in question.
             */
 
             //------------------------- Initialization ------------------------- 
@@ -41,24 +53,16 @@ namespace ConsoleApp1
 
             Console.WriteLine("Would you like to search for only crit frames?"); //Self-Explanatory
             critAsk = Console.ReadLine(); //Gets answer for crit frame question
-            if (critAsk.IndexOf("y", 0, 1) == 0) //Checks if the first character of critAsk is "y" and sets critSearch to true if it is
-            {
-                critSearch = true;
-            }
-            if (critAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "Y" and sets critSearch to true if it is
+            if (critAsk.IndexOf("y", 0, 1) == 0 || critAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "y" or "Y" and sets critSearch to true if it is
             {
                 critSearch = true;
             }
 
-            if (critSearch == true)
+            if (critSearch == true) //Only asks this question if you answered yes to the critAsk question
             {
                 Console.WriteLine("Would you like to search for only max roll crit frame pairs?"); //Self-Explanatory
                 rollAsk = Console.ReadLine(); //Gets answer for roll frame question
-                if (rollAsk.IndexOf("y", 0, 1) == 0) //Checks if the first character of critAsk is "y" and sets rollSearch to true if it is
-                {
-                    rollSearch = true;
-                }
-                if (rollAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "Y" and sets rollSearch to true if it is
+                if (rollAsk.IndexOf("y", 0, 1) == 0 || rollAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "y" or "Y" and sets rollSearch to true if it is
                 {
                     rollSearch = true;
                 }
@@ -75,17 +79,17 @@ namespace ConsoleApp1
             int firstCalc = BaseNumOne * InitSeed + BaseNumTwo; //Calculates the first RNG result
             int repeated = 1; //Initializes the amount of times the loop has been repeated
             string hexResult = firstCalc.ToString("X8"); //Converts firstCalc back to hex
-            if (critSearch == false)
+            if (critSearch == false) //Checks if critSearch is false and if so, just runs the program with no changes
             {
                 Console.WriteLine("1: 0x" + hexResult); //Prints the hex value of firstCalc
             }
             if (critSearch == true && hexResult.IndexOf("0", 3, 1) == 3) //Checks if critSearch is set to true and checks if the 4th character in hexResult is "0"
             {
-                if (rollSearch == false)
+                if (rollSearch == false) //Checks if rollSearch is set to false and if so, runs the normal critSearch loop
                 {
                     Console.WriteLine("1: 0x" + hexResult);
                 }
-                if (rollSearch == true)
+                if (rollSearch == true) //Checks if rollSearch is set to true and if so, runs a subcalculation in order to check if the second value in part of a pair also meets the requirements
                 {
                     subCalc = BaseNumOne * firstCalc + BaseNumTwo;
                     subLoopCount++;
@@ -95,14 +99,14 @@ namespace ConsoleApp1
                         subHex = subCalc.ToString("X8");
                         subLoopCount++;
                     }
-                    if (subHex.IndexOf("0", 3, 1) == 3)
+                    if (subHex.IndexOf("0", 3, 1) == 3) //Only prints if both the initial and final frame in a pair fit the requirements
                     {
                         Console.WriteLine(repeated + ": 0x" + hexResult);
                         finalFrame = repeated + 5;
                         Console.WriteLine(finalFrame + ": 0x" + subHex);
                         Console.WriteLine();
                     }
-                    subLoopCount = 1;
+                    subLoopCount = 1; //Resets the subcalculation loop counter to 1
                 }
             }
 
@@ -117,11 +121,11 @@ namespace ConsoleApp1
                 }
                 if (critSearch == true && hexResult.IndexOf("0", 3, 1) == 3)
                 {
-                    if (rollSearch == false)
+                    if (rollSearch == false) //Checks if rollSearch is set to false and if so, runs the normal critSearch loop
                     {
                         Console.WriteLine(repeated + ": 0x" + hexResult);
                     }
-                    if (rollSearch == true)
+                    if (rollSearch == true) //Checks if rollSearch is set to true and if so, runs a subcalculation in order to check if the second value in part of a pair also meets the requirements
                     {
                         subCalc = BaseNumOne * firstCalc + BaseNumTwo;
                         subLoopCount++;
@@ -131,14 +135,14 @@ namespace ConsoleApp1
                             subHex = subCalc.ToString("X8");
                             subLoopCount++;
                         }
-                        if (subHex.IndexOf("0", 3, 1) == 3)
+                        if (subHex.IndexOf("0", 3, 1) == 3) //Only prints if both the initial and final frame in a pair fit the requirements
                         {
                             Console.WriteLine(repeated + ": 0x" + hexResult);
                             finalFrame = repeated + 5;
                             Console.WriteLine(finalFrame + ": 0x" + subHex);
                             Console.WriteLine();
                         }
-                        subLoopCount = 1;
+                        subLoopCount = 1; //Resets the subcalculation loop counter to 1
                     }
                 }
             }
