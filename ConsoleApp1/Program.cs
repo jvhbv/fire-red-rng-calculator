82330e4e25826757aced381febc91b9b64ae1fdb
Schwamii <joecool3336@gmail.com>
2020-06-16 21:20:06 -0400

    Making the program more readable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,12 @@ namespace ConsoleApp1
 {
     public class calc
     {
+        private static bool YesOrNo(string question)
+        {
+            Console.WriteLine(question);
+            string answer = Console.ReadLine();
+            return char.ToLower(answer[0]) == 'y';
+        }
 
         public static void Main(string[] args)
         {
@@ -34,28 +40,27 @@ namespace ConsoleApp1
             string rngBaseNumOne = "41C64E6D"; //First number in RNG equation
             string rngBaseNumTwo = "6073"; //Second fixed number in RNG equation
             bool critSearch = false; //Initializes a boolean for the question of whether to search for crit frames
-            string critAsk; //Initializes the string that will be read from to set critSearch
             bool rollSearch = false; //Initializes a boolean for the question of whether to search for rolls after crits
-            string rollAsk; //Initializes the string that will be read from to set rollSearch
             int subCalc; //Initializes an integer that's used in the rollSearch loops
             int subLoopCount = 1; //Initializes another integer that's used in the rollSearch loops
             string subHex = "FFFFFFFF"; //Initializes a string that's used in the rollSearch loops
             int finalFrame; //Initializes an integer that's used if a match is found on the rollSearch loops
-            int parseAttempt; //Initializes an integer that's mandatory for TryParse
+            //int parseAttempt; //Initializes an integer that's mandatory for TryParse
             CultureInfo provider = CultureInfo.InvariantCulture; //Creates an IFormatProvider variable so TryParse can work with the hex values on initial seed input
             bool initSeedParse = false; //Initializes a boolean for the TryParse while loop for the initial seed
             bool repeatInputParse = false; //Initializes a boolean for the TryParse while loop for the number of times to repeat
-            string saveAsk; //Initializes the string that will be read from to set save
             bool save = false; //Initializes a boolean for the question of whether to save the results
             bool encounterSearch = false;
-            string encounterAsk;
             bool noEncounters = false;
-            string noEncounterAsk;
             string firstTileFrame = "";
-            int tileFrame;
+            int tileFrame = 0;
             bool tileFrameParse = false;
             int looped = 0;
             int encounterCalc;
+            int moveType = 0;
+            string moveAsk;
+            int InitSeed = 0;
+            int repeatTimes = 0;
             //------------------------- End of initialization ------------------------- 
 
 
@@ -65,38 +70,50 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("Enter the initial seed hex value: "); //Self-Explanatory
                 rngInitSeed = Console.ReadLine(); //Sets initial seed to the value that was input
-                initSeedParse = int.TryParse(rngInitSeed, NumberStyles.HexNumber, provider, out parseAttempt); //Attempts to parse the input, setting initSeedParse to true if it succeeds
-                if (initSeedParse == false)
-                {
-                    Console.WriteLine("Please enter a hex value.");
-                    Console.WriteLine();
-                }
+                initSeedParse = int.TryParse(rngInitSeed, NumberStyles.HexNumber, provider, out InitSeed); //Attempts to parse the input, setting initSeedParse to true if it succeeds
+                if (initSeedParse) { break; }
+                Console.WriteLine("Please enter a hex value.");
+                Console.WriteLine();
             }
 
-            Console.WriteLine("Would you like to search for only frames that produce an encounter? (Ruby/Sapphire only)"); //Self-Explanatory
-            encounterAsk = Console.ReadLine(); //Gets answer for encounter question
-            if (encounterAsk.IndexOf("y", 0, 1) == 0 || encounterAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of encounterAsk is "y" or "Y" and sets encounterSearch to true if it is
+            if (YesOrNo("Would you like to search for only frames that produce an encounter? (Ruby/Sapphire only)"))
             {
                 encounterSearch = true;
             }
 
             if (encounterSearch == true)
             {
-                Console.WriteLine("Do you want to calculate how many tiles you can move based on the RNG frame you entered the first tile?");
-                noEncounterAsk = Console.ReadLine();
-                if (noEncounterAsk.IndexOf("y", 0, 1) == 0 || noEncounterAsk.IndexOf("Y", 0, 1) == 0)
+                if (YesOrNo("Do you want to calculate how many tiles you can move based on the RNG frame you entered the first tile?"))
                 {
                     noEncounters = true;
+
+                    Console.WriteLine("Are you (w)alking, (r)unning, on the (a)cro bike, or on the (m)ach bike? (acro not implemented yet)");
+                    moveAsk = Console.ReadLine();
+                    if (char.ToLower(moveAsk[0]) == 'w')
+                    {
+                        moveType = 17;
+                    }
+                    if (char.ToLower(moveAsk[0]) == 'r')
+                    {
+                        moveType = 9;
+                    }
+                    if (char.ToLower(moveAsk[0]) == 'a')
+                    {
+                        moveType = 17;
+                    }
+                    if (char.ToLower(moveAsk[0]) == 'm')
+                    {
+                        moveType = 5;
+                    }
+
                     while (tileFrameParse == false)
                     {
                         Console.WriteLine("Enter the first RNG frame that you walked into a tile.");
                         firstTileFrame = Console.ReadLine();
-                        tileFrameParse = int.TryParse(firstTileFrame, out parseAttempt);
-                        if (tileFrameParse == false)
-                        {
-                            Console.WriteLine("Please enter an integer.");
-                            Console.WriteLine();
-                        }
+                        tileFrameParse = int.TryParse(firstTileFrame, out tileFrame);
+                        if (tileFrameParse) { break; }
+                        Console.WriteLine("Please enter an integer.");
+                        Console.WriteLine();
                     }
                 }
             }
@@ -107,37 +124,28 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine("Enter number of times to repeat: "); //Self-Explanatory
                     repeat = Console.ReadLine(); //Gets number of times to repeat
-                    repeatInputParse = int.TryParse(repeat, out parseAttempt); //Attempts to parse the input, setting repeatInputParse to true if it succeeds
-                    if (repeatInputParse == false)
-                    {
-                        Console.WriteLine("Please enter an integer.");
-                        Console.WriteLine();
-                    }
+                    repeatInputParse = int.TryParse(repeat, out repeatTimes); //Attempts to parse the input, setting repeatInputParse to true if it succeeds
+                    if (repeatInputParse) { break; }
+                    Console.WriteLine("Please enter an integer.");
+                    Console.WriteLine();
                 }
 
                 if (encounterSearch == false)
                 {
-                    Console.WriteLine("Would you like to search for only crit frames?"); //Self-Explanatory
-                    critAsk = Console.ReadLine(); //Gets answer for crit frame question
-                    if (critAsk.IndexOf("y", 0, 1) == 0 || critAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "y" or "Y" and sets critSearch to true if it is
+                    if (YesOrNo("Would you like to search for only crit frames?"))
                     {
                         critSearch = true;
                     }
-
                     if (critSearch == true) //Only asks this question if you answered yes to the critAsk question
                     {
-                        Console.WriteLine("Would you like to search for only max roll crit frame pairs?"); //Self-Explanatory
-                        rollAsk = Console.ReadLine(); //Gets answer for roll frame question
-                        if (rollAsk.IndexOf("y", 0, 1) == 0 || rollAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of critAsk is "y" or "Y" and sets rollSearch to true if it is
+                        if (YesOrNo("Would you like to search for only max roll crit frame pairs?"))
                         {
                             rollSearch = true;
                         }
                     }
                 }
 
-                Console.WriteLine("Would you like to save the results to a txt file? (significantly slower at the moment)"); //Self-Explanatory
-                saveAsk = Console.ReadLine(); //Gets answer for crit frame question
-                if (saveAsk.IndexOf("y", 0, 1) == 0 || saveAsk.IndexOf("Y", 0, 1) == 0) //Checks if the first character of saveAsk is "y" or "Y" and sets save to true if it is
+                if (YesOrNo("Would you like to save the results to a txt file? (significantly slower at the moment)"))
                 {
                     save = true;
                 }
@@ -147,7 +155,7 @@ namespace ConsoleApp1
 
 
             //------------------------- Calculation and display ------------------------- 
-            int InitSeed = Int32.Parse(rngInitSeed, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngInitSeed
+            //int InitSeed = Int32.Parse(rngInitSeed, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngInitSeed
             int BaseNumOne = Int32.Parse(rngBaseNumOne, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumOne
             int BaseNumTwo = Int32.Parse(rngBaseNumTwo, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumTwo
             int firstCalc = BaseNumOne * InitSeed + BaseNumTwo; //Calculates the first RNG result
@@ -156,7 +164,7 @@ namespace ConsoleApp1
 
             if (noEncounters)
             {
-                tileFrame = Int32.Parse(firstTileFrame);
+                //tileFrame = Int32.Parse(firstTileFrame);
                 while (repeated < tileFrame) //Finds RNG value on RNG frame given
                 {
                     firstCalc = BaseNumOne * firstCalc + BaseNumTwo;
@@ -169,7 +177,7 @@ namespace ConsoleApp1
                 while (encounterCalc >= 320)
                 {
                     subLoopCount++;
-                    while (subLoopCount <= 17)
+                    while (subLoopCount <= moveType)
                     {
                         subCalc = BaseNumOne * subCalc + BaseNumTwo;
                         subHex = subCalc.ToString("X8");
@@ -177,7 +185,7 @@ namespace ConsoleApp1
                     }
                     subLoopCount = 0;
 
-                    string hexDigits = subHex.Substring(0, 4); //Makes a string that is the first 4 characters of hexResult
+                    string hexDigits = subHex.Substring(0, 4); //Makes a string that is the first 4 characters of subHex
                     int encounterVar = Int32.Parse(hexDigits, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of hexDigits
                     encounterCalc = encounterVar % 2880; //Sets an integer equal to encounterVar mod 2880
                     if (encounterCalc >= 320)
@@ -190,7 +198,7 @@ namespace ConsoleApp1
 
             if (noEncounters == false)
             {
-                int repeatTimes = Int32.Parse(repeat); //Sets an integer equal to the parsed value of repeat
+                //int repeatTimes = Int32.Parse(repeat); //Sets an integer equal to the parsed value of repeat
                 if (critSearch == false) //Checks if critSearch is false and if so, just runs the program with no changes
                 {
                     if (encounterSearch == false)
@@ -230,7 +238,7 @@ namespace ConsoleApp1
                     {
                         subCalc = BaseNumOne * firstCalc + BaseNumTwo;
                         subLoopCount++;
-                        while (subLoopCount <= 5)
+                        while (subLoopCount <= 7) //Can be changed to 5 for FR/LG, 7 for Ruby/Sapphire, not sure about emerald
                         {
                             subCalc = BaseNumOne * subCalc + BaseNumTwo;
                             subHex = subCalc.ToString("X8");
@@ -239,7 +247,7 @@ namespace ConsoleApp1
                         if (subHex.IndexOf("0", 3, 1) == 3) //Only prints if both the initial and final frame in a pair fit the requirements
                         {
                             Console.WriteLine(repeated + ": 0x" + hexResult);
-                            finalFrame = repeated + 5;
+                            finalFrame = repeated + 7; //Can be changed to 5 for FR/LG, 7 for Ruby/Sapphire, not sure about emerald
                             Console.WriteLine(finalFrame + ": 0x" + subHex);
                             Console.WriteLine();
                             if (save)
@@ -296,7 +304,7 @@ namespace ConsoleApp1
                         {
                             subCalc = BaseNumOne * firstCalc + BaseNumTwo;
                             subLoopCount++;
-                            while (subLoopCount <= 5)
+                            while (subLoopCount <= 7) //Can be changed to 5 for FR/LG, 7 for Ruby/Sapphire, not sure about emerald
                             {
                                 subCalc = BaseNumOne * subCalc + BaseNumTwo;
                                 subHex = subCalc.ToString("X8");
@@ -305,7 +313,7 @@ namespace ConsoleApp1
                             if (subHex.IndexOf("0", 3, 1) == 3) //Only prints if both the initial and final frame in a pair fit the requirements
                             {
                                 Console.WriteLine(repeated + ": 0x" + hexResult);
-                                finalFrame = repeated + 5;
+                                finalFrame = repeated + 7; //Can be changed to 5 for FR/LG, 7 for Ruby/Sapphire, not sure about emerald
                                 Console.WriteLine(finalFrame + ": 0x" + subHex);
                                 Console.WriteLine();
                                 if (save)
