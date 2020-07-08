c1653b3f4f64b7f388cfe9e470dd90e8283bd282
joe <joecool3336@gmail.com>
2020-07-08 11:22:59 -0400

    adding a way to search for initial seeds

---
+++ source/repos/gen3RNGcalc/gen3RNGcalc/MainWindow.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.IO;
+using System.ComponentModel;
+using System.Threading;
+using System.Diagnostics;
+using System.Reflection;
+
+/*
+    Pokémon Generation 3 RNG Calculator
+    Copyright (C) 2020  Joseph Van Horn
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace gen3RNGcalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string rngInitSeed;
+        string rngBaseNumOne = "41C64E6D"; //First number in RNG equation
+        string rngBaseNumTwo = "6073"; //Second fixed number in RNG equation
+        int InitSeed;
+        int repeatTimes = 0;
+        int subCalc; //Initializes an integer that's used in the rollSearch loops
+        int subLoopCount = 1; //Initializes another integer that's used in the rollSearch loops
+        int rollCalculation;
+        string subHex = "FFFFFFFF";
+        int rollParsed = 0;
+        int gameVar = 0;
+        int finalFrame;
+        int minimumRepeat = 0;
+        bool rollParse = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool? critSearch = crits.IsChecked;
+            bool? rollSearch = rolls.IsChecked;
+            bool? gameSelect1 = game1.IsChecked; //The Ruby/Sapphire/Emerald button does nothing right now, pretty much only there for looks.
+
+            if (gameSelect1 == true)
+            {
+                gameVar = 5;
+            }
+            else { gameVar = 7; }
+
+            rngInitSeed = seedInput.Text;
+            bool initSeedParse = int.TryParse(rngInitSeed, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out InitSeed); //Attempts to parse the input, setting initSeedParse to true if it succeeds
+            if (initSeedParse == false)
+            {
+                exception.Text = "Please enter a hexadecimal value for the initial seed.";
+            }
+
+            string repeat = repeatInput.Text;
+            bool repeatInputParse = int.TryParse(repeat, out repeatTimes); //Attempts to parse the input, setting repeatInputParse to true if it succeeds
+            if (repeatInputParse == false)
+            {
+                exception.Text = "Please enter an integer for the maximum number of times to repeat.";
+            }
+
+            string minRepeat = repeatMinimum.Text;
+            bool minInputParse = int.TryParse(minRepeat, out minimumRepeat); //Attempts to parse the input, setting minInputParse to true if it succeeds
+            if (minInputParse == false)
+            {
+                exception.Text = "Please enter an integer for the minimum frame you want displayed.";
+            }
+
+            bool minleqmax = minimumRepeat <= repeatTimes;
+            if (minleqmax == false)
+            {
+                exception.Text = "Please make sure the minimum number to display is less than or equal to the maximum.";
+            }
+            
+            if (rollSearch == true)
+            {
+                string rollSelect = rollMin.Text;
+                rollParse = int.TryParse(rollSelect, out rollParsed); //Attempts to parse the input, setting rollParse to true if it succeeds
+                if (rollParse == false)
+                {
+                    exception.Text = "Please enter an integer for the roll you want to search for.";
+                }
+            }
+
+            if (initSeedParse && repeatInputParse && minInputParse && minleqmax && rollSearch == false ^ (rollSearch == true && rollParse))
+            {
+                Results win2 = new Results();
+                win2.Show();
+                int BaseNumOne = int.Parse(rngBaseNumOne, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumOne
+                int BaseNumTwo = int.Parse(rngBaseNumTwo, NumberStyles.HexNumber); //Sets an integer equal to the parsed value of rngBaseNumTwo
+                int firstCalc = BaseNumOne * InitSeed + BaseNumTwo; //Calculates the first RNG result
+                int repeated = 1; //Initializes the amount of times the loop has been repeated
+                string hexResult = firstCalc.ToString("X8"); //Converts firstCalc back to hex
+
+                if (critSearch == false && rollSearch == false) //Checks if critSearch is false and if so, just runs the program with no changes
+                {
+                    if (minimumRepeat <= repeated)
+                    {
+                        win2.output.Text = "1: 0x" + hexResult;
+                    }
+                }
+                else if (critSearch == true && hexResult[3] == '0') //Checks if critSearch is set to true and if the 4th character in hexResult is 0
+                {
+                    if (rollSearch == false) //Checks if rollSearch is set to false and if so, runs the normal critSearch loop
+                    {
+                        if (minimumRepeat <= repeated)
+                        {
+                            win2.output.Text = "1: 0x" + hexResult;
+                        }
+                    }
+                    if (rollSearch == true) //Checks if rollSearch is set to true and if so, runs a subcalculation in order to check if the second value in part of a pair also meets the requirements
+                    {
+                        subCalc = BaseNumOne * firstCalc + BaseNumTwo;
+                        subLoopCount++;
+                        while (subLoopCount <= gameVar)
+                        {
+                            subCalc = BaseNumOne * subCalc + BaseNumTwo;
+                            subHex = subCalc.ToString("X8");
+                            subLoopCount++;
+                        }
+                        rollCalculation = int.Parse(subHex.Substring(3, 1), NumberStyles.HexNumber);
+                        if (rollCalculation <= rollParsed)
+                        {
+                            if (minimumRepeat <= repeated)
+                            {
+                                win2.output.Text = repeated + ": 0x" + hexResult;
+                                finalFrame = repeated + gameVar;
+                                win2.output.Text = win2.output.Text + "\n" + finalFrame + ": 0x" + subHex + "\n";
+                            }
+                        }
+                        subLoopCount = 1; //Resets the subcalculation loop counter to 1
+                    }
+                }
+                else if (rollSearch == true && critSearch == false && int.Parse(hexResult.Substring(3,1), NumberStyles.HexNumber) <= rollParsed)
+                {
+                    win2.output.Text = repeated + ": 0x" + hexResult;
+                }
+
+                while (repeated < repeatTimes) //Loop function
+                {
+                    firstCalc = BaseNumOne * firstCalc + BaseNumTwo; //Does the equation again
+                    hexResult = firstCalc.ToString("X8");
+                    repeated++;
+                    if (critSearch == false && rollSearch == false) //Checks if critSearch is false and if so, just runs the program with no changes
+                    {
+                        if (minimumRepeat <= repeated)
+                        {
+                            win2.output.Text = win2.output.Text + "\n" + repeated + ": 0x" + hexResult;
+                            win2.output.Height = win2.output.Height + 14;
+                        }
+                    }
+                    else if (critSearch == true && hexResult[3] == '0') //Checks if critSearch is set to true and if the 4th character in hexResult is 0
+                    {
+                        if (rollSearch == false) //Checks if rollSearch is set to false and if so, runs the normal critSearch loop
+                        {
+                            if (minimumRepeat <= repeated)
+                            {
+                                win2.output.Text = win2.output.Text + "\n" + repeated + ": 0x" + hexResult;
+                                win2.output.Height = win2.output.Height + 14;
+                            }
+                        }
+                        if (rollSearch == true) //Checks if rollSearch is set to true and if so, runs a subcalculation in order to check if the second value in part of a pair also meets the requirements
+                        {
+                            subCalc = BaseNumOne * firstCalc + BaseNumTwo;
+                            subLoopCount++;
+                            while (subLoopCount <= gameVar)
+                            {
+                                subCalc = BaseNumOne * subCalc + BaseNumTwo;
+                                subHex = subCalc.ToString("X8");
+                                subLoopCount++;
+                            }
+                            rollCalculation = int.Parse(subHex.Substring(3, 1), NumberStyles.HexNumber);
+                            if (rollCalculation <= rollParsed)
+                            {
+                                if (minimumRepeat <= repeated)
+                                {
+                                    win2.output.Text = win2.output.Text + "\n" + repeated + ": 0x" + hexResult;
+                                    finalFrame = repeated + gameVar;
+                                    win2.output.Text = win2.output.Text + "\n" + finalFrame + ": 0x" + subHex + "\n";
+                                    win2.output.Height = win2.output.Height + 42;
+                                }
+                            }
+                            subLoopCount = 1; //Resets the subcalculation loop counter to 1
+                        }
+                    }
+                    else if (rollSearch == true && critSearch == false && int.Parse(hexResult.Substring(3, 1), NumberStyles.HexNumber) <= rollParsed)
+                    {
+                        win2.output.Text = win2.output.Text + "\n" + repeated + ": 0x" + hexResult;
+                        win2.output.Height = win2.output.Height + 14;
+                    }
+                }
+            }
+        }
+
+        private void Help(object sender, RoutedEventArgs e)
+        {
+            Help help = new Help();
+            help.Show();
+        }
+
+        private void License(object sender, RoutedEventArgs e)
+        {
+            var directory = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var file = System.IO.Path.Combine(directory, "COPYING.txt");
+            Process.Start(file);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            rsSeedFinder seedFinder = new rsSeedFinder();
+            seedFinder.Show();
+            Close();
+        }
+    }
+}
---
+++ source/repos/gen3RNGcalc/gen3RNGcalc/rsSeedFinder.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace gen3RNGcalc
+{
+    /// <summary>
+    /// Interaction logic for rsSeedFinder.xaml
+    /// </summary>
+    public partial class rsSeedFinder : Window
+    {
+        public rsSeedFinder()
+        {
+            InitializeComponent();
+        }
+
+        private void Go(object sender, RoutedEventArgs e)
+        {
+            error.Text = ""; //resets the error log upon running again
+            bool hoursParse = double.TryParse(hour.Text, out double hours);
+            if (!hoursParse) //makes sure you don't enter something that will crash the application
+            {
+                error.Text = error.Text + "Please enter an integer for hours.\n";
+            }
+            if (hours > 23) //makes sure you enter a number of hours less than or equal to the maximum in a day
+            {
+                error.Text = error.Text + "Please make sure the hours entered are not greater than 23.\n";
+            }
+            bool minutesParse = double.TryParse(minute.Text, out double minutes);
+            if (!minutesParse) //makes sure you don't enter something that will crash the application
+            {
+                error.Text = error.Text + "Please enter an integer for minutes.\n";
+            }
+            if (minutes > 59) //makes sure you enter a number of minutes less than or equal to the maximum in an hour
+            {
+                error.Text = error.Text + "Please make sure the minutes entered are not greater than 59.";
+            }
+            if (minutesParse && hoursParse && minutes <= 59 && hours <= 23) //verifies that the inputs received passed all checks leading up to this point
+            {
+                var date = dateSelected.SelectedDate.Value.Date.AddMinutes(minutes); //creates a variable for the date received and sets the minutes for the date to the minutes input
+                date = date.AddHours(hours); //sets the hours for the date to the hours input
+                int result = (int)(date.Subtract(new DateTime(2000, 1, 1))).TotalMinutes; //gets the total number of minutes passed since January 1st, 2000
+                int maxSeed = int.Parse("FFFF", NumberStyles.HexNumber); //just here to make the math easier to follow
+                int remainder = result % maxSeed; //finalizes the number of minutes that actually matter for the seed generation
+                int defaultSeed = int.Parse("5A0", NumberStyles.HexNumber); //also here to make the math easier to follow, it just declares 05A0 as the default initial seed
+                int finalResult = defaultSeed + remainder; //finalizes the calculation of the initial seed
+                string resultOut = finalResult.ToString("X4"); //converts the final result to the 16 bit hexadecimal number representing the initial seed
+                seed.Text = resultOut; //outputs the final result as the aforementioned 16 bit hexadecimal number
+                copy.Visibility = Visibility.Visible; //makes the copy button visible
+            }
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win2 = new MainWindow(); //creates a new instance of MainWindow
+            win2.seedInput.Text = seed.Text; //sets the seed input field to the one generated
+            win2.Show(); //shows the new instance of MainWindow
+            Close(); //closes the seed finder
+        }
+    }
+}
